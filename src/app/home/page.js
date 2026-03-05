1d29f5a561e7fff2c99bb483080ff49adcec5be5
Haroon <pk.rasheed.haroon@gmail.com>
2026-03-05 12:14:11 +0500

    changes in homepage

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -3,6 +3,7 @@
 const Page = () => {
     return (
         <>
+            <h1>Hello, Haroon!</h1>
             <div class="grid grid-flow-col grid-rows-1 gap-2 text-center">
                 <div class="col-span-3 bg-amber-400">01</div>
                 <div class="col-span-3 bg-amber-800">02</div>
